f811fcdf2f058f544b6b7231b43a0697d464e959
devarajit <devarajit@gmail.com>
2021-07-08 22:38:54 +0530

    test

---
+++ springbootmongo/src/main/java/com/code/vega/dto/SpringbootMongodbDemoApplication.java
@@ -0,0 +1,11 @@
+package com.code.vega.dto;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMongodbDemoApplication.class, args);
+    }
+}
